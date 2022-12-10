43390235e043213777fca00e46f5af0680cda168
ShuffleZZZ <mihpihnaty@yandex.ru>
2022-12-10 18:50:40 +0300

    :sparkles: day 10

---
+++ src/Day10.kt
@@ -0,0 +1,47 @@
+private const val LAST_STEP = 220
+private const val SCREEN_WIDTH = 40
+private const val SCREEN_SIZE = 6 * SCREEN_WIDTH
+
+
+private fun cycleSignals(input: List<String>): List<Int> {
+    val signals = MutableList(2 * input.size + 1) { 1 }
+
+    var cycle = 0
+    for (i in input.indices) {
+        signals[cycle + 1] = signals[cycle]
+
+        val instruction = input[i].split(" ")
+        if (instruction.first() == "addx") {
+            signals[++cycle + 1] = signals[cycle] + instruction.last().toInt()
+        }
+
+        cycle++
+    }
+
+    return signals
+}
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val signals = cycleSignals(input)
+
+        return (SCREEN_WIDTH / 2..LAST_STEP step SCREEN_WIDTH).fold(0) { acc, i -> acc + i * signals[i - 1] }
+    }
+
+    fun part2(input: List<String>): String {
+        val signals = cycleSignals(input).take(SCREEN_SIZE)
+
+        return signals.chunked(SCREEN_WIDTH) { line ->
+            line.mapIndexed { ind, signal -> if (signal in ind - 1..ind + 1) '#' else '.' }.joinToString("")
+        }.joinToString("\n")
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day10_test")
+    check(part1(testInput) == 13_140)
+    println(part2(testInput))
+
+    val input = readInput("Day10")
+    println(part1(input))
+    println(part2(input))
+}
